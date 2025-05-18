48ad3f6ccacc53244ed46716e42de314a5f6a1fd
Marius Ratnikas <marius.ratnikas@gmail.com>
2025-05-18 22:40:38 +0300

    fix expiments time

--- site/js/time.js
+++ site/js/time.js
@@ -1,9 +1,7 @@
 function calculate() {
   const today = new Date();
-  const pastDate = new Date('1991-07-01');
+  const pastDate = new Date('1991');
 
-  const diffTime = today - pastDate;
-  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
   const diffYears = today.getFullYear() - pastDate.getFullYear();
 
   document.getElementById('result').innerText = `${diffYears}`;
